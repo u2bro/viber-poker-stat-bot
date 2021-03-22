01a33895dc6ed791bedd791f956967299454c963
Alex Khramenkov <akhramenkov@gmail.com>
2021-03-22 22:34:30 +0200

    add admin commands

--- src/ViberPokerBot/webhook.php
+++ src/ViberPokerBot/webhook.php
@@ -12,13 +12,23 @@ require_once __DIR__ . '/Lib/Logger.php';
 
 const TRACK_SUBSCRIBE = 'subscribe';
 
-const COMMAND_REFRESH_USERS = '/refresh_users';
-const COMMAND_COMMANDS = '/commands';
+const COMMAND_REFRESH_MEMBERS = 'refresh-members';
+const COMMAND_IDS = 'ids';
+const COMMAND_ADMINS = 'admins';
+const COMMAND_ADMIN_ADD = 'admin-add';
+const COMMAND_ADMIN_REMOVE = 'admin-remove';
+const COMMAND_COMMANDS = 'commands';
+const COMMAND_USERS = 'users';
 const COMMANDS_ADMIN = [
-    COMMAND_REFRESH_USERS,
+    COMMAND_IDS,
+    COMMAND_REFRESH_MEMBERS,
+    COMMAND_ADMIN_ADD,
+    COMMAND_ADMIN_REMOVE,
 ];
 const COMMANDS_REGULAR = [
     COMMAND_COMMANDS,
+    COMMAND_ADMINS,
+    COMMAND_USERS,
 ];
 
 
@@ -42,33 +52,33 @@ Logger::log(($_SERVER ? 'Server: ' . json_encode($_SERVER) . PHP_EOL : '')
     . ($request ? 'Input: ' . $request . PHP_EOL : ''));
 
 $userStorage = new UserStorage();
-if ($input['event'] == 'webhook') {
+if ($input['event'] === 'webhook') {
     $webhook_response['status'] = 0;
     $webhook_response['status_message'] = "ok";
     $webhook_response['event_types'] = 'delivered';
     echo json_encode($webhook_response);
     die;
-} elseif ($input['event'] == "subscribed") {
+} elseif ($input['event'] === "subscribed") {
     $newUse = new \stdClass();
     $newUse->id = $input['user']['id'] ?? null;
     $newUse->name = $input['user']['name'] ?? null;
     $newUse->avatar = $input['user']['avatar'] ?? null;
-    $newUse->role = 'user';
+    $newUse->role = UserStorage::ROLE_ADMIN;
     $newUse->isSubscribed = true;
     $userStorage->updateUser($newUse);
-} elseif ($input['event'] == "unsubscribed") {
+} elseif ($input['event'] === "unsubscribed") {
     $userStorage->setSubscribe($input['user_id'], false);
-} elseif ($input['event'] == "conversation_started") {
+} elseif ($input['event'] === "conversation_started") {
     $newUse = new \stdClass();
     $newUse->id = $input['user']['id'] ?? null;
     $newUse->name = $input['user']['name'] ?? null;
     $newUse->avatar = $input['user']['avatar'] ?? null;
-    $newUse->role = 'user';
+    $newUse->role = UserStorage::ROLE_USER;
     $newUse->isSubscribed = false;
     $userStorage->updateUser($newUse);
     $data['receiver'] = $newUse->id;
     $data['sender']['name'] = 'bot';
-    $data['text'] = "Welcome to the Poker Uzh bot!\n\n*Send any message* to start conversation and see list of available commands.";
+    $data['text'] = "Welcome to the *Poker Uzh bot!*\n\n♠️♣️♥️♦️\n\n*Send any message* to start conversation and see list of available commands.";
     $data['type'] = 'text';
     $data['keyboard']['Type'] = 'keyboard';
 //    $data['keyboard']['DefaultHeight'] = true;
@@ -88,21 +98,91 @@ if ($input['event'] == 'webhook') {
     $data['min_api_version'] = 1;
     jsonResponse($data);
     die();
-} elseif ($input['event'] == "message") {
-    $text = $input['message']['text'] ?? '';
+} elseif ($input['event'] === "message") {
+    $text = lcfirst($input['message']['text'] ?? '');
     $track = $input['message']['tracking_data'] ?? '';
     $senderId = $input['sender']['id'] ?? null;
     $isAdmin = $userStorage->isUserAdmin($senderId);
+    $data = [];
+    $data['receiver'] = $senderId;
+    $data['sender']['name'] = 'bot';
+    $data['type'] = 'text';
+    $data['tracking_data'] = 'tracking_data';
+    $data['min_api_version'] = 1;
     if ($isAdmin) {
-        if ($text === COMMAND_REFRESH_USERS) {
+        if ($text === COMMAND_REFRESH_MEMBERS) {
             $dataApp = callApi('https://chatapi.viber.com/pa/get_account_info');
 
             $userStorage->updateUsers($dataApp->members);
-            $text = 'Current users: ';
-            $users = $userStorage->getUsers();
-            foreach ($users as $key => $user) {
-                $text .= $user->name . (!empty($users[$key + 1]) ? ', ' : '.');
+            $text = 'Current members: ';
+            foreach ($dataApp->members as $key => $user) {
+                $text .= $user->name . (!empty($dataApp->members[$key + 1]) ? ', ' : '.');
             }
+        }
+        if ($text === COMMAND_IDS) {
+            if(!isSupperAdmin($senderId)) {
+                $data['text'] = '*Error* : this command allowed only for superadmins';
+                callApi('https://chatapi.viber.com/pa/send_message', $data);
+                die();
+            }
+            $data['text'] = 'User ids: ';
+            callApi('https://chatapi.viber.com/pa/send_message', $data);
+            foreach ($userStorage->getUsers() as $key => $user) {
+                $data['text'] = $user->name . ' ' . $user->id;
+                callApi('https://chatapi.viber.com/pa/send_message', $data);
+            }
+            die();
+        }
+        if (strpos($text, COMMAND_ADMIN_ADD) === 0 || strpos($text, COMMAND_ADMIN_REMOVE) === 0) {
+            if(!isSupperAdmin($senderId)) {
+                $data['text'] = '*Error* : this command allowed only for superadmins';
+                callApi('https://chatapi.viber.com/pa/send_message', $data);
+                die();
+            }
+            $id = explode(':', $text)[1] ?? null;
+            if (!$id) {
+                $data['text'] = 'User ids: ';
+                callApi('https://chatapi.viber.com/pa/send_message', $data);
+                foreach ($userStorage->getUsers() as $key => $user) {
+                    $data['text'] = $user->name . ' admin_' . (strpos($text, COMMAND_ADMIN_ADD) === 0 ? 'add:' : 'remove:') . $user->id;
+                    callApi('https://chatapi.viber.com/pa/send_message', $data);
+                }
+
+            }
+            $res = $userStorage->setRole($id, strpos($text, COMMAND_ADMIN_ADD) === 0 ? UserStorage::ROLE_ADMIN : UserStorage::ROLE_USER);
+            if ($res) {
+                $text = 'Done. Current admins: ';
+                $admins = [];
+                foreach ($userStorage->getUsers() as $user) {
+                    if ($user->role === UserStorage::ROLE_ADMIN) {
+                        $admins[] = $user;
+                    }
+                }
+                foreach ($admins as $key => $admin) {
+                    $text .= $admin->name . (!empty($admins[$key + 1]) ? ', ' : '.');
+                }
+            } else {
+                $text = 'Not found user with id: ' . $id;
+            }
+        }
+    }
+    if ($text === COMMAND_ADMINS) {
+        $text = 'Admins: ';
+        $admins = [];
+        foreach ($userStorage->getUsers() as $user) {
+            if ($user->role === UserStorage::ROLE_ADMIN) {
+                $admins[] = $user;
+            }
+        }
+        foreach ($admins as $key => $admin) {
+            $text .= $admin->name . (!empty($admins[$key + 1]) ? ', ' : '.');
+        }
+    }
+    if ($text === COMMAND_USERS) {
+        $text = 'Users: ';
+        $users = $userStorage->getUsers();
+        foreach ($users as $key => $user) {
+            $text .= $user->name . (!empty($users[$key + 1]) ? ', ' : '.');
         }
     }
     if ($track === TRACK_SUBSCRIBE) {
@@ -110,12 +190,12 @@ if ($input['event'] == 'webhook') {
         $newUse->id = $input['sender']['id'] ?? null;
         $newUse->name = $input['sender']['name'] ?? null;
         $newUse->avatar = $input['sender']['avatar'] ?? null;
-        $newUse->role = 'user';
+        $newUse->role = UserStorage::ROLE_USER;
         $newUse->isSubscribed = true;
         $userStorage->updateUser($newUse);
     }
 
-    if ($text === $input['message']['text'] ?? '') {
+    if ($text === lcfirst($input['message']['text'] ?? '')) {
         $commands = COMMANDS_REGULAR;
         if ($isAdmin) {
             $commands = array_merge($commands, COMMANDS_ADMIN);
@@ -126,16 +206,7 @@ if ($input['event'] == 'webhook') {
         }
     }
 
-    /* when a user message is received */
-    $type = $input['message']['type'];
-    $senderName = $input['sender']['name'];
-
-    $data['receiver'] = $senderId;
-    $data['sender']['name'] = 'bot';
     $data['text'] = $text;
-    $data['type'] = 'text';
-    $data['tracking_data'] = 'tracking_data';
-    $data['min_api_version'] = 1;
 
     callApi('https://chatapi.viber.com/pa/send_message', $data);
 }
@@ -168,6 +239,17 @@ function callApi(string $url, array $data = null)
     $body = substr($response, $header_size);
 
     return json_decode($response);
+}
+
+function isSupperAdmin(string $id = null): bool
+{
+    $dataApp = callApi('https://chatapi.viber.com/pa/get_account_info');
+    foreach ($dataApp->members as $key => $user) {
+        if ($user->id === $id) {
+            return true;
+        }
+    }
+    return false;
 }
 
 //"subscribed",
